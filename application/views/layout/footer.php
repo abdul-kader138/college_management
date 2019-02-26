90cfe10affdec103ba6c75207dd637ced36cdda4
Kader <babu313136@gmail.com>
2019-02-27 01:32:29 +0600

    Code cleaning

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,7 +1,10 @@
-<footer class="footer">
-    <div class="pull-right">
+<div class="clearfix"></div>
+<footer class="footer" >
+    <div class="pull-left" style="padding-top: 10px;padding-bottom: 10px;">
         <p class="white"><?php echo $this->global_setting->brand_footer ? $this->global_setting->brand_footer : '' ?>
-            <br><a style="color: white" target="_blank" href="http://www.oneclicksolutionbd.com">Developed By : OneClick Solution BD</a></p>
+        </p>
     </div>
-    <div class="clearfix"></div>
+    <div class="pull-right" style="padding-top: 10px;padding-bottom: 10px;">
+            <p><a style="color: white" target="_blank" href="http://www.oneclicksolutionbd.com">Developed By : OneClick Solution BD</a></p>
+    </div>
 </footer>
